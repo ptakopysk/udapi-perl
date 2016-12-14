bb52f01313b144634d0c7968c18268d19da418eb
rosa <rosa@ufal.mff.cuni.cz>
2016-12-14 17:22:12 +0100

    cs, dir

--- lib/Udapi/Block/Rur/POSfactors.pm
+++ lib/Udapi/Block/Rur/POSfactors.pm
@@ -3,27 +3,58 @@ use Udapi::Core::Common;
 extends 'Udapi::Core::Block';
 
 has_ro dir => ( isa=>Bool, default => 0 );
+has_ro num => ( isa=>Bool, default => 0 );
+has cs => ( is=>'rw', default => 1 );
+
+sub print_word {
+    my ($self, $form, $position, $node) = @_;
+
+    my $upos;
+    if (!defined $node) {
+        $upos = 'EOS';
+    } elsif ($node->ord == 0) {
+        $upos = 'BOS';
+    } else {
+        $upos = $node->upos;
+    }
+
+    if ($self->dir) {
+        $upos .= ($position > 0 ? '+' : '-');
+    }
+
+    if ($self->num) {
+        $upos .= abs($position);
+    }
+
+    print $form . "\t" . $upos . "\n";
+
+    return ;
+}
 
 sub process_node {
     my ($self, $node) = @_;
 
-    my $prev_upos = "BOS";
-    if ($node->ord > 1) {
-        $prev_upos = $node->prev_node()->upos;
-    }
+    my $form = $node->form;
 
-    my $next_upos = "EOS";
-    if (defined $node->next_node()) {
-        $next_upos = $node->next_node()->upos;
+    my $prev_node = $node->prev_node();
+    for (my $position = -1; $position >= -$self->cs; $position--) {
+        $self->print_word($form, $position, $prev_node);
+        if ($prev_node->ord == 0) {
+            last;
+        } else {
+            $prev_node = $prev_node->prev_node();
+        }
     }
-
-    if ($self->dir) {
-        $prev_upos = $prev_upos . '-';
-        $next_upos = $next_upos . '+';
+    
+    my $next_node = $node->next_node();
+    for (my $position = 1; $position <= $self->cs; $position++) {
+        $self->print_word($form, $position, $next_node);
+        if (!defined $next_node) {
+            last;
+        } else {
+            $next_node = $next_node->next_node();
+        }
     }
-
-    print $node->form . "\t" . $prev_upos . "\n";
-    print $node->form . "\t" . $next_upos . "\n";
 
     return ;
 }
